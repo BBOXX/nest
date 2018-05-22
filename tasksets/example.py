c49646c33d5c6044ee8fbd50161028941225dc54
g.punter <g.punter@bboxx.co.uk>
2018-05-22 14:28:06 +0100

    Adding example file

---
+++ tasksets/example.py
@@ -0,0 +1,29 @@
+from locust import TaskSet, task
+
+class Model(TaskSet):
+    weight = 0
+    def on_start(self):
+        """Set up before running tasks. 
+        
+        For example:
+        * Log in & save token
+        * Retrieve bulk information needed for other tasks
+
+        """
+        return
+
+    def on_stop(self):
+        """Teardown: unclaim resources e.g. claimed user.
+
+        """
+
+        return
+    
+    @task(5) # task decorator with relative weight of executing the task
+    def model_action(self):
+        """Codified behaviour of a particular action this model may perform
+        e.g. registering a customer
+
+        """
+        
+        return
