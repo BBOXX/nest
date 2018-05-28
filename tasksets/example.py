5fc64ee75fa407d88dca6dca6dcc8fe377850259
g.punter <g.punter@bboxx.co.uk>
2018-05-28 15:34:57 +0100

    PEP8 changes

--- tasksets/example.py
+++ tasksets/example.py
@@ -1,10 +1,12 @@
 from locust import TaskSet, task, HttpLocust
 
+
 class Model(TaskSet):
     weight = 0
+
     def on_start(self):
-        """Set up before running tasks. 
-        
+        """Set up before running tasks.
+
         For example:
         * Log in & save token
         * Retrieve bulk information needed for other tasks
@@ -18,16 +20,17 @@ class Model(TaskSet):
         """
 
         return
-    
-    @task(5) # task decorator with relative weight of executing the task
+
+    # task decorator with relative weight of executing the task
+    @task(5) 
     def model_action(self):
         """Codified behaviour of a particular action this model may perform
         e.g. registering a customer
 
         """
         self.client.get("/")
-        
         return
+
 
 class ModelLocust(HttpLocust):
     host = "http://127.0.0.1:8089"
